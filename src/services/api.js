e6d5d3dbf22dc7021e8debf93c30f16d26df8c70
Pradeeph <deephikamaths@gmail.com>
2026-03-19 13:01:12 +0530

    fixed api base url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://rbac-backend-s5lz.onrender.com/api",
 });
 
 // Add token automatically
